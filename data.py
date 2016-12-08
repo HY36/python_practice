771faa75dd0b66765fa9f49673858cb042f76266
huoyan <895350445@qq.com>
2016-12-09 00:45:47 +0800

    正态分布检测

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
+# -*- coding:utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
-
-N = 1000
+from scipy.stats import shapiro
+from scipy.stats import anderson
+from scipy.stats import normaltest
 
 
 def binomial():
@@ -25,12 +28,33 @@ def binomial():
 
 
 def normal():
+    N = 1000
     normal_value = np.random.normal(size=N)
-    dummy, bins, dummy = plt.hist(normal_value, np.sqrt(N), normed=True, lw=1)
+    dummy, bins, dummy = plt.hist(normal_value, int(np.sqrt(N)), normed=True, lw=1)
     sigma = 1
     mu = 0
     plt.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-(bins - mu) ** 2 / (2 * sigma ** 2)), lw=2)
     plt.show()
 
+
+def normal_test():
+    flutrends = np.loadtxt("goog_flutrends.csv", delimiter=',', usecols=(1, ), skiprows=1,
+                           converters={1: lambda s: float(s or 0)},unpack=True)
+    N = len(flutrends)
+    normal_values = np.random.normal(size=N)
+    zero_values = np.zeros(N)
+
+    # print shapiro(normal_values)
+    # print shapiro(zero_values)
+    # print shapiro(flutrends)
+
+    # print anderson(normal_values)
+    # print anderson(zero_values)
+    # print anderson(flutrends)
+
+    print normaltest(normal_values)
+    print normaltest(zero_values)
+    print normaltest(flutrends)
+
 if __name__ == '__main__':
-    normal()
+    normal_test()
