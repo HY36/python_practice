6748fec632f40fe73eb3733934b776b234555f84
huoyan <895350445@qq.com>
2016-12-10 21:20:06 +0800

    pandas dataframe数据结构的了解

--- data/data.py
+++ data/data.py
@@ -57,4 +57,4 @@ def normal_test():
     print(normaltest(flutrends))
 
 if __name__ == '__main__':
-    binomial()
+    normal()
--- pandas_demo/df_demo.py
+++ pandas_demo/df_demo.py
@@ -1,3 +1,11 @@
 from pandas.io.parsers import read_csv
 
 df = read_csv("WHO_first9cols.csv")
+
+print(df)
+print(df.shape)
+print(len(df))
+print(df.columns)
+print(df.dtypes)
+print(df.index)
+print(df.values)
